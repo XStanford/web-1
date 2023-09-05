49b86f513350d7fb9e3df47b7166ce04af60881e
Stanford <378469429@qq.com>
2023-09-05 11:48:36 +0800

    项目初始化

---
+++ src/components/HomeTable.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="homeBox">ss1</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.homeBox{
+    width: 100%;
+    height: 100%;
+}
+</style>
